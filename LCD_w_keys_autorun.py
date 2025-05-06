2e210873586499ccea337de851fea6db8f86e388
Oleksiy Kolesnikov <klsnkv@klsnkv-pro.local>
2025-05-06 01:59:06 -0500

    buttons not drawn

--- LCD_w_keys_autorun.py
+++ LCD_w_keys_autorun.py
@@ -62,42 +62,42 @@ try:
                 disp.ShowImage(image1)
         # with canvas(device) as draw:
         if disp.digital_read(disp.GPIO_KEY_UP_PIN ) == 0: # button is released
-            draw.polygon([(20, 20), (30, 2), (40, 20)], outline=255, fill=0xff00)  #Up        
+            # draw.polygon([(20, 20), (30, 2), (40, 20)], outline=255, fill=0xff00)  #Up        
         else: # button is pressed:
             refresh_display = True
             draw.polygon([(20, 20), (30, 2), (40, 20)], outline=255, fill=0)  #Up filled
             print ("Up")
             
         if disp.digital_read(disp.GPIO_KEY_LEFT_PIN) == 0: # button is released
-            draw.polygon([(0, 30), (18, 21), (18, 41)], outline=255, fill=0xff00)  #left           
+            # draw.polygon([(0, 30), (18, 21), (18, 41)], outline=255, fill=0xff00)  #left           
         else: # button is pressed:
             refresh_display = True
             draw.polygon([(0, 30), (18, 21), (18, 41)], outline=255, fill=0)  #left filled
             print ("left") 
             
         if disp.digital_read(disp.GPIO_KEY_RIGHT_PIN) == 0: # button is released
-            draw.polygon([(60, 30), (42, 21), (42, 41)], outline=255, fill=0xff00) #right        
+            # draw.polygon([(60, 30), (42, 21), (42, 41)], outline=255, fill=0xff00) #right        
         else: # button is pressed:
             refresh_display = True
             draw.polygon([(60, 30), (42, 21), (42, 41)], outline=255, fill=0) #right filled
             print ("right")
             
         if disp.digital_read(disp.GPIO_KEY_DOWN_PIN) == 0: # button is released
-            draw.polygon([(30, 60), (40, 42), (20, 42)], outline=255, fill=0xff00) #down        
+            # draw.polygon([(30, 60), (40, 42), (20, 42)], outline=255, fill=0xff00) #down        
         else: # button is pressed:
             refresh_display = True
             draw.polygon([(30, 60), (40, 42), (20, 42)], outline=255, fill=0) #down filled
             print ("down")
             
         if disp.digital_read(disp.GPIO_KEY_PRESS_PIN) == 0: # button is released
-            draw.rectangle((20, 22,40,40), outline=255, fill=0xff00) #center         
+            # draw.rectangle((20, 22,40,40), outline=255, fill=0xff00) #center         
         else: # button is pressed:
             refresh_display = True
             draw.rectangle((20, 22,40,40), outline=255, fill=0) #center filled
             print ("center")
             
         if disp.digital_read(disp.GPIO_KEY1_PIN) == 0: # button is released
-            draw.ellipse((70,0,90,20), outline=255, fill=0xff00) #A button        
+            # draw.ellipse((70,0,90,20), outline=255, fill=0xff00) #A button        
         else: # button is pressed:
             refresh_display = True
             draw.ellipse((70,0,90,20), outline=255, fill=0) #A button filled
@@ -112,7 +112,7 @@ try:
 
             
         if disp.digital_read(disp.GPIO_KEY2_PIN) == 0: # button is released
-            draw.ellipse((100,20,120,40), outline=255, fill=0xff00) #B button]        
+            # draw.ellipse((100,20,120,40), outline=255, fill=0xff00) #B button]        
         else: # button is pressed:
             refresh_display = True
             draw.ellipse((100,20,120,40), outline=255, fill=0) #B button filled
@@ -120,7 +120,7 @@ try:
             print ("KEY2")
             
         if disp.digital_read(disp.GPIO_KEY3_PIN) == 0: # button is released
-            draw.ellipse((70,40,90,60), outline=255, fill=0xff00) #A button        
+            # draw.ellipse((70,40,90,60), outline=255, fill=0xff00) #A button        
         else: # button is pressed:
             refresh_display = True
             draw.ellipse((70,40,90,60), outline=255, fill=0) #A button filled
