7f6e13cc6fa67967d1298cf36b14601434b5f27f
Oleksiy Kolesnikov <klsnkv@klsnkv-pro.local>
2025-05-06 01:37:52 -0500

    joystic

--- LCD_w_keys_autorun.py
+++ LCD_w_keys_autorun.py
@@ -52,30 +52,35 @@ try:
         if disp.digital_read(disp.GPIO_KEY_UP_PIN ) == 0: # button is released
             draw.polygon([(20, 20), (30, 2), (40, 20)], outline=255, fill=0xff00)  #Up        
         else: # button is pressed:
+            refresh_display = True
             draw.polygon([(20, 20), (30, 2), (40, 20)], outline=255, fill=0)  #Up filled
             print ("Up")
             
         if disp.digital_read(disp.GPIO_KEY_LEFT_PIN) == 0: # button is released
             draw.polygon([(0, 30), (18, 21), (18, 41)], outline=255, fill=0xff00)  #left           
         else: # button is pressed:
+            refresh_display = True
             draw.polygon([(0, 30), (18, 21), (18, 41)], outline=255, fill=0)  #left filled
             print ("left") 
             
         if disp.digital_read(disp.GPIO_KEY_RIGHT_PIN) == 0: # button is released
             draw.polygon([(60, 30), (42, 21), (42, 41)], outline=255, fill=0xff00) #right        
         else: # button is pressed:
+            refresh_display = True
             draw.polygon([(60, 30), (42, 21), (42, 41)], outline=255, fill=0) #right filled
             print ("right")
             
         if disp.digital_read(disp.GPIO_KEY_DOWN_PIN) == 0: # button is released
             draw.polygon([(30, 60), (40, 42), (20, 42)], outline=255, fill=0xff00) #down        
         else: # button is pressed:
+            refresh_display = True
             draw.polygon([(30, 60), (40, 42), (20, 42)], outline=255, fill=0) #down filled
             print ("down")
             
         if disp.digital_read(disp.GPIO_KEY_PRESS_PIN) == 0: # button is released
             draw.rectangle((20, 22,40,40), outline=255, fill=0xff00) #center         
         else: # button is pressed:
+            refresh_display = True
             draw.rectangle((20, 22,40,40), outline=255, fill=0) #center filled
             print ("center")
             
